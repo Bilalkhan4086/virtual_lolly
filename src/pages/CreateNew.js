6d9924747bd61349ed38705bf5180abc654f969e
Muhammad Bilal <Bscs.f18.m16.a@gmail.com>
2021-05-02 11:08:59 +0500

    Update CreateNew.js

--- src/pages/CreateNew.js
+++ src/pages/CreateNew.js
@@ -21,9 +21,23 @@ const CreateNew = () => {
                             <Lolly cTop={cTop} cMiddle={cMiddle} cBottom={cBottom} />
                         </div>
                         <div className="flavorSelector">
-                            <label className="flavorPickerLabel" style={{ borderColor: cTop, boxShadow: `${cTop} 0 0 8px`, }} htmlFor="flavorPicker"><input type="color" onChange={(e) => { setcTop(e.currentTarget.value) }} className="flavorPicker" value={cTop} /></label>
-                            <label className="flavorPickerLabel" style={{ borderColor: cMiddle, boxShadow: `${cMiddle} 0 0 8px`, }} htmlFor="a"><input type="color" onChange={(e) => { setcMiddle(e.currentTarget.value) }} className="flavorPicker" value={cMiddle} /></label>
-                            <label className="flavorPickerLabel" style={{ borderColor: cBottom, boxShadow: `${cBottom} 0 0 8px`, }} htmlFor="a"><input type="color" onChange={(e) => { setcBottom(e.currentTarget.value) }} className="flavorPicker" value={cBottom} /></label>
+                            <label 
+                            className="flavorPickerLabel" 
+                            style={{ borderColor: cTop, boxShadow: `${cTop} 0 0 8px`, }} 
+                            htmlFor="flavorPicker">
+                                <input type="color" onChange={(e) => { setcTop(e.currentTarget.value) }} className="flavorPicker" value={cTop} />
+                            </label>
+                            <label 
+                            className="flavorPickerLabel" 
+                            style={{ borderColor: cMiddle, boxShadow: `${cMiddle} 0 0 8px`, }}
+                            htmlFor="flavorPicker">
+                                <input type="color" onChange={(e) => { setcMiddle(e.currentTarget.value) }} className="flavorPicker" value={cMiddle} />
+                            </label>
+                            <label className="flavorPickerLabel" 
+                            style={{ borderColor: cBottom, boxShadow: `${cBottom} 0 0 8px`, }} 
+                            htmlFor="flavorPicker">
+                                <input type="color" onChange={(e) => { setcBottom(e.currentTarget.value) }} className="flavorPicker" value={cBottom} />
+                            </label>
                         </div>
                     </div>
 <div>
