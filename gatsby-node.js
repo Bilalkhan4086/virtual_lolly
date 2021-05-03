4e45712be525cbb9fd56e0b28de183f0a19108d5
Muhammad Bilal <Bscs.f18.m16.a@gmail.com>
2021-05-03 06:47:31 +0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,42 @@
-const path = require("path");
+// const path = require("path");
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-  const { data } = await graphql(`
-    query MyQuery {
-        Lollies {
-          getLollies {
-            cTop
-            from
-            to
-            message
-            cMiddle
-            cBottom
-            linkPath
-          }
-        }
-      }
-      `);
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions;
+//   const { data } = await graphql(`
+//     query MyQuery {
+//         Lollies {
+//           getLollies {
+//             cTop
+//             from
+//             to
+//             message
+//             cMiddle
+//             cBottom
+//             linkPath
+//           }
+//         }
+//       }
+//       `);
 
 
-  data.Lollies.getLollies.forEach((d) => {
-    // console.log(d.first, "in")
-    createPage({
-      path: `lolly/${d.linkPath}`,
-      component: path.resolve(`./src/templates/index.js`),
-      context: d,
-    });
-  });
-};
+//   data.Lollies.getLollies.forEach((d) => {
+//     // console.log(d.first, "in")
+//     createPage({
+//       path: `lolly/${d.linkPath}`,
+//       component: path.resolve(`./src/templates/index.js`),
+//       context: d,
+//     });
+//   });
+// };
 
-exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions;
+// exports.onCreatePage = async ({ page, actions }) => {
+//   const { createPage } = actions;
 
-  if (page.path.match(/^\/lollies/)) {
-    page.matchPath = "/lollies/*";
+//   if (page.path.match(/^\/lollies/)) {
+//     page.matchPath = "/lollies/*";
 
-    // Update the page.
+//     // Update the page.
 
-    createPage(page);
-  }
-};
+//     createPage(page);
+//   }
+// };
