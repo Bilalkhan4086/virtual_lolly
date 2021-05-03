b3c91e2028027bcab0e4d2eb78e6564d9ce657af
Muhammad Bilal <Bscs.f18.m16.a@gmail.com>
2021-05-03 08:15:04 +0500

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,96 @@
+// const path = require("path");
+
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions;
+//   const { data } = await graphql(`
+//     query MyQuery {
+//         Lollies {
+//           getLollies {
+//             cTop
+//             from
+//             to
+//             message
+//             cMiddle
+//             cBottom
+//             linkPath
+//           }
+//         }
+//       }
+//       `);
+
+
+//   data.Lollies.getLollies.forEach((d) => {
+//     // console.log(d.first, "in")
+//     createPage({
+//       path: `lolly/${d.linkPath}`,
+//       component: path.resolve(`./src/templates/index.js`),
+//       context: d,
+//     });
+//   });
+// };
+
+// exports.onCreatePage = async ({ page, actions }) => {
+//   const { createPage } = actions;
+
+//   if (page.path.match(/^\/lollies/)) {
+//     page.matchPath = "/lollies/*";
+
+//     // Update the page.
+
+//     createPage(page);
+//   }
+// };
+
+
 const path = require("path");
+const faunadb = require("faunadb");
+const q = faunadb.query;
 
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions;
-  const { data } = await graphql(`
-    query MyQuery {
-        Lollies {
-          getLollies {
-            cTop
-            from
-            to
-            message
-            cMiddle
-            cBottom
-            linkPath
-          }
-        }
-      }
-      `);
+require("dotenv").config();
 
-
-  data.Lollies.getLollies.forEach((d) => {
-    // console.log(d.first, "in")
-    createPage({
-      path: `lolly/${d.linkPath}`,
-      component: path.resolve(`./src/templates/index.js`),
-      context: d,
-    });
-  });
-};
-
-exports.onCreatePage = async ({ page, actions }) => {
-  const { createPage } = actions;
-
-  if (page.path.match(/^\/lollies/)) {
-    page.matchPath = "/lollies/*";
-
-    // Update the page.
-
-    createPage(page);
+exports.createPages = async function ({ actions, graphql }) {
+  try {
+    if (process.env.FAUNADB_SERVER_SECRET) {
+      var client = new faunadb.Client({
+        secret: process.env.FAUNADB_SERVER_SECRET,
+      });
+      const result = await client.query(
+        q.Map(
+          q.Paginate(q.Documents(q.Collection('Lollies'))),
+          q.Lambda(x => q.Get(x))
+        )
+      );
+      console.log(result);
+      result.data.forEach((lolly) => {
+        actions.createPage({
+          path: `lolly/${lolly.ref.id}`,
+          component: require.resolve(`./src/components/LollyTemplate.tsx`),
+          context: {
+            // Data passed to context is available
+            // in pageContext props of the template component
+            id: lolly.ref.id,
+            from: lolly.data.from,
+            to: lolly.data.to,
+            message: lolly.data.message,
+            CTop: lolly.data.cTop,
+            cMiddle: lolly.data.cMiddle,
+            cBottom: lolly.data.cBottom,
+            linkPath: lolly.data.linkPath,
+          },
+        });
+      });
+    }
+  } catch (error) {
+    console.log(error);
   }
+  // actions.createPage({
+  //     path: `lolly/${lolly.id}`,
+  //     component: require.resolve(`./src/templates/dynamic-page.tsx`),
+  //     context: {
+  //         // Data passed to context is available
+  //         // in pageContext props of the template component
+  //         name: "Zia",
+  //      },
+  // });
+  // console.log("End of Gatsby Node File");
 };
