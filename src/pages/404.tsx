788dbffef77ac49a37fdb0da30cc10fcafe71a43
Muhammad Bilal <Bscs.f18.m16.a@gmail.com>
2021-05-03 23:12:00 +0500

    updating 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,8 +6,8 @@ import Layout from "../components/layout"
 const NotFoundPage = () => (
   <Layout>
     {/* <Seo title="404: Not found" /> */}
-    <h1 style={{color:"white"}}>404: Not Found</h1>
-    <p style={{color:"white"}}>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1 className="style" style={{color:"white"}}>Sorry Your lolly is not freezzed yet</h1>
+    <p className="style" style={{color:"white"}}>You just wait a while and quickly copy the url.</p>
   </Layout>
 )
 
